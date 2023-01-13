913d096d61abec660cccf4caac89e19405486426
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2023-01-14 03:25:51 +0530

    Integrate Uffizzi

--- src/core/main.go
+++ src/core/main.go
@@ -66,8 +66,7 @@ import (
 	"github.com/goharbor/harbor/src/pkg/scan/dao/scanner"
 	pkguser "github.com/goharbor/harbor/src/pkg/user"
 	"github.com/goharbor/harbor/src/pkg/version"
-	"github.com/goharbor/harbor/src/server/v2.0/handler"
-	"github.com/goharbor/harbor/src/server/v2.0/route"
+	"github.com/goharbor/harbor/src/server"
 )
 
 const (
